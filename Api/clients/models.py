faf3daf891be239d4f20dd6ca2eebab2dce423eb
Sanmerson <san_merson3@hotmail.com>
2023-03-25 23:11:01 -0300

    feat: add clients route

--- Api/clients/models.py
+++ Api/clients/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
+class Clients(models.Model):
+    class Meta:
+        ordering = ["id"]
+
+    name = models.CharField(max_length=120)
+    email = models.EmailField(max_length=120)
+    phone = models.CharField(max_length=20)
+    create_at = models.DateTimeField(auto_now_add=True)
 # Create your models here.
