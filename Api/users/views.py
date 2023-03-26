c111ade9326d534adbe20d9f402fdb9ad2fd6883
Sanmerson <san_merson3@hotmail.com>
2023-03-25 22:28:55 -0300

    feat: add users route

--- Api/users/views.py
+++ Api/users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from .models import User
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from .serializers import UserSerializer
+from rest_framework import generics
+
 
 # Create your views here.
+class UserView(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
